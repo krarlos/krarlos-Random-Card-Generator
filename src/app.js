ad874737b12ce7f2378d892bd1341a5e3b1e4051
Carlos Eduardo <127683028+krarlos@users.noreply.github.com>
2023-11-13 02:00:29 +0000

    avance

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
+
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+let number = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "R", "J", "A"];
+let cards = ["♣", "♥", "♦", "♠"];
+cards;
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  let random_numbers = number[Math.floor(Math.random() * number.length)];
+  let random_card = cards[Math.floor(Math.random() * cards.length)];
+
+  
+  document.querySelector(".top-icon").innerHTML = random_card;
+  document.querySelector(".under-icon").innerHTML = random_card;
+  document.querySelector(".numero").innerHTML = random_numbers;
+  if (
+    document.querySelector(".top-icon").innerHTML &&
+    document.querySelector(".under-icon").innerHTML == "♥"
+  ) {
+    document.querySelector(".top-icon").classList.add("red");
+    document.querySelector(".under-icon").classList.add("red");
+  }
 };
